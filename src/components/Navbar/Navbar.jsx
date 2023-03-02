584e04a16d459aab02517f2ce8a7b20c1288f380
KleberMayC <klebercoelhogt@gmail.com>
2023-03-01 21:20:17 -0300

    Criando o componente navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../Navbar/navbar.css';
+import Logo from '../../assets/images/dnc-logo.png';
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <h1>sadsad</h1>
+        <img src={Logo} alt="" />
+    </div>
+  )
+}
+
+export default Navbar
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
+import Navbar from "../../components/Navbar/Navbar";
 import '../Home/home.css'
 
 export function Home(){
   return(<>
+  <Navbar />
   <section className="section-home">
   <h1>Home Page</h1>
   <button><Link to={`/`}>Voltar</Link></button>
