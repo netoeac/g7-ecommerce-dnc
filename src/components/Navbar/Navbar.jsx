80e554cd228003c29face87bb109c87fc82350da
KleberMayC <klebercoelhogt@gmail.com>
2023-03-02 21:03:55 -0300

    Parte 1 da navegação pronta falta a responsividade

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
 import '../Navbar/navbar.css';
 import Logo from '../../assets/images/dnc-logo.png';
+import Cart from '../../assets/images/cart.png';
+import Search  from '../../assets/images/search.png';
+
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-        <h1>sadsad</h1>
-        <img src={Logo} alt="" />
+        <div className="nav">
+            <img className='logo' src={Logo} alt="" />
+           
+            <input className='input' type="search" name="search" id="" placeholder='O que você está procurando?'/>
+            
+            <a href=""><img className='cart' src={Cart} alt="" /></a>
+        </div>
+
+        <div className='list'>
+
+                    <a href="">Novidades</a>
+                    <a href="">Jogos</a>
+                    <a href="">Video Games</a>
+                    <a href="">Mesas Gamer</a>
+                    <a href="">Promoções</a>
+                    <a href="">Atendimento</a>
+
+        </div>
+
+
     </div>
   )
 }
