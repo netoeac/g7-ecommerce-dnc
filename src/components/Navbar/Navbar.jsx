fac3ea8cae677b4114b1199d889ffb467d212358
KleberMayC <klebercoelhogt@gmail.com>
2023-03-02 21:42:18 -0300

    Atualizações da navegação

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import '../Navbar/navbar.css';
 import Logo from '../../assets/images/dnc-logo.png';
 import Cart from '../../assets/images/cart.png';
 import Search  from '../../assets/images/search.png';
+import Menu from '../../assets/images/menu.png';
 
 
 const Navbar = () => {
@@ -10,23 +11,24 @@ const Navbar = () => {
     <div className='navbar'>
         <div className="nav">
             <img className='logo' src={Logo} alt="" />
+            
            
             <input className='input' type="search" name="search" id="" placeholder='O que você está procurando?'/>
             
             <a href=""><img className='cart' src={Cart} alt="" /></a>
+            
         </div>
 
-        <div className='list'>
-
-                    <a href="">Novidades</a>
-                    <a href="">Jogos</a>
-                    <a href="">Video Games</a>
-                    <a href="">Mesas Gamer</a>
-                    <a href="">Promoções</a>
-                    <a href="">Atendimento</a>
-
+        <div id='itens' className='list'>
+            <ul>
+                <li><a href="">Novidades</a></li>
+                <li><a href="">Jogos</a></li>
+                <li><a href="">Video Games</a></li>
+                <li><a href="">Mesas Gamer</a></li>
+                <li><a href="">Promoções</a></li>
+                <li><a href="">Atendimento</a></li>
+            </ul>
         </div>
-
 
     </div>
   )
