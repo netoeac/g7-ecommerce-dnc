01f6f5bdd5bce5393607e695daf27f436849f0ba
FabricioBontadini <117490461+FabricioBontadini@users.noreply.github.com>
2023-03-07 00:40:56 -0300

    Estruturação de Pastas e Arquivos de Imagens

---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,9 @@
+import "./index.scss";
+
+const register = () => {
+    return (
+        <div>register</div>
+    )
+}
+
+export default register
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
+import "./index.scss";
 import Login from "./views/Login/Login";
 
 import { Home } from "./views/Home/Home";
