20bfd9adac9c8832c188640e12923e82e86e73cd
FabricioBontadini <117490461+FabricioBontadini@users.noreply.github.com>
2023-03-08 20:35:53 -0300

    Finalização Cadastro Produtos

--- src/mock/products.mock.js
+++ src/mock/products.mock.js
@@ -13,7 +13,7 @@ export const PRODUCTS_MOCK = [
     {
         id: 2,
         imgPath: "produto_2.png",
-        imgPathDetail: "",
+        imgPathDetail: "../../assets/images/produto_2.png",
         title: "Console Xbox Series S 512GB SSD",
         infoProducts: "Para você que é fã de games e está a procura de um novo console para uma nova experiência, precisa conhecer o Xbox Series S da Microsoft na cor branca. Lançado em 2020, ele tem SSD de 512GB para armazenar diversos jogos e 1 controle sem fio que deixa você livre para jogar. Use apenas com jogos digitais. O console não reproduz discos físicos. Inclui o download gratuito dos jogos Fortnite, Rocket League e Fall Guys. O conteúdo é entregue diretamente ao seu console durante a configuração e nenhum código é necessário.",
         price: "R$ 2.516,55",
@@ -24,7 +24,7 @@ export const PRODUCTS_MOCK = [
     {
         id: 3,
         imgPath: "produto_3.png",
-        imgPathDetail: "",
+        imgPathDetail: "../../assets/images/produto_3.png",
         title: "Nintendo Switch com Jogo",
         infoProducts: "Nintendo Switch é o mais novo console de videogame doméstico da Nintendo. Além de proporcionar diversão em casa para um ou mais jogadores, o console Nintendo Switch pode ser transformando em um console portátil num piscar de olhos.",
         price: "R$ 2.045,07",
@@ -35,7 +35,7 @@ export const PRODUCTS_MOCK = [
     {
         id: 4,
         imgPath: "produto_4.png",
-        imgPathDetail: "",
+        imgPathDetail: "../../assets/images/produto_4.png",
         title: "Jogo God of War Playstation",
         infoProducts: "Sua vingança contra os deuses do Olimpo agora é passado, em God of War, Kratos vive como um homem comum nas terras dos deuses Nórdicos, ensinando seu filho a sobreviver a todos os perigos que aparecerem por sua frente. Kratos está empenhado em passar todos seus conhecimentos ao seu filho, que está determinado a conquistar o respeito de seu pai. Finalmente o assassino de deuses tem a oportunidade de corrigir seus erros do passado.",
         price: "R$ 109,90",
@@ -46,11 +46,24 @@ export const PRODUCTS_MOCK = [
     {
         id: 5,
         imgPath: "produto_5.png",
-        imgPathDetail: "",
+        imgPathDetail:"../../assets/images/produto_5.png",
         title: "Jogo Uncharted: The Nathan Drake Collection",
         infoProducts: "Para você que é fã de games e está a procura de um novo console para uma nova experiência, precisa conhecer o Xbox Series S da Microsoft na cor branca. Lançado em 2020, ele tem SSD de 512GB para armazenar diversos jogos e 1 controle sem fio que deixa você livre para jogar. Use apenas com jogos digitais. O console não reproduz discos físicos. Inclui o download gratuito dos jogos Fortnite, Rocket League e Fall Guys. O conteúdo é entregue diretamente ao seu console durante a configuração e nenhum código é necessário.",
         price: "R$ 99,90",
         colors: "null",
         colorsView: "null",
+    },
+
+    {
+        id: 6,
+        imgPath: "produto_6.png",
+        imgPathDetail: "../../assets/images/produto_6.png",
+        title: "Jogo Gran Turismo",
+        infoProducts: "Nesse jogo a experiência definitiva de corridas está de volta e melhor do que nunca, somente no PlayStation 4 Gran Turismo Sport é a primeira experiência de corrida do mundo recriada do zero para trazer competições internacionais online autorizadas pelo maior órgão controlador de corridas automobilísticas internacionais, a FIA (Federação Internacional de Automobilismo).",
+        price: "R$ 104,90",
+        colors: "null",
+        colorsView: "#null",
     }
+
+
 ]
