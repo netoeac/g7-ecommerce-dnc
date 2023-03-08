b6b568e5a9e44a84910bc7db7d4c320e330d7e8c
FabricioBontadini <fabricio@escrivera.com.br>
2023-03-08 17:28:08 -0300

    Criando banco produtos

---
+++ src/mock/products.mock.js
@@ -0,0 +1,11 @@
+export const PRODUCTS_MOCK = [
+    {
+        id: 1,
+        imgPath: "produto_1.png",
+        imgPathDetail: "",
+        title: "Console Xbox Series S 512GB SSD",
+        infoProducts: "Para você que é fã de games e está a procura de um novo console para uma nova experiência, precisa conhecer o Xbox Series S da Microsoft na cor branca. Lançado em 2020, ele tem SSD de 512GB para armazenar diversos jogos e 1 controle sem fio que deixa você livre para jogar. Use apenas com jogos digitais. O console não reproduz discos físicos. Inclui o download gratuito dos jogos Fortnite, Rocket League e Fall Guys. O conteúdo é entregue diretamente ao seu console durante a configuração e nenhum código é necessário.",
+        price:"R$ 2.516,55",
+    }
+    
+]
