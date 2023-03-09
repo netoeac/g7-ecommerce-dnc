2b24d3ab5284abb109c7e787f630d685c41b41b4
netoeac <netoeac.dev@gmail.com>
2023-03-08 21:26:38 -0300

    add images examples for banner and banner component added

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,9 +1,13 @@
 import { Link } from "react-router-dom";
+import Banner from "../../components/Banner/Banner";
 import "./index.scss";
 
 const Home = () => {
   return (
     <section className="section-home">
+      <div className="banner">
+        <Banner />
+      </div>
       <h1>We have to develop the Home Page</h1>
       <button><Link to={`/products`}>Ir para produtos</Link></button>
       <button><Link to={`/`}>Voltar</Link></button>
